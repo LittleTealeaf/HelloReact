a1ecf65e53f5b35967eb9e79e272034d12a0387b
Thomas Kwashnak <ThomasKwashnak@gmail.com>
2022-03-12 10:11:24 -0500

    something

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         WHYYYYYYYYOIJAWEFOIJAWEFOAWEOJIFWAEF
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          It seems that it will not load for some apparent reason
         </a>
       </header>
     </div>
